700717f0e5751766c2a1d7102f6a3b2db98bc57a
Vomartins <vm.omartins@outlook.com>
2024-09-30 16:02:34 +0200

    Size of vecRes fixed, vecSol introduced for error calculation

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #endif
 
 #include <iostream>
+#include <algorithm>
 //#include "rocsparse-ilu0.hpp"
 //#include "well-matrices.hpp"
 #include <dune/common/version.hh>
@@ -71,74 +72,19 @@ int main(int argc, char ** argv)
   std::vector<int> Dcols, Drows;
   std::vector<unsigned int> Bcols, Ccols, Brows, Crows;
 
-
   loadBCRSMatrixVectors(Dvals, Dcols, Drows, "matrix-D.bin");
   loadBCRSMatrixVectors(Bvals, Bcols, Brows, "matrix-B.bin");
   loadBCRSMatrixVectors(Cvals, Ccols, Crows, "matrix-C.bin");
 
-  std::cout << "Dvals: ";
-  std::cout << size(Dvals) << " " <<std::endl;
-  //for (const auto& val : Dvals) std::cout << val << " ";
-  //std::cout << std::endl;
-  std::cout << "Dcols: ";
-  std::cout << size(Dcols) << " " <<std::endl;
-  for (const auto& val : Dcols) std::cout << val << " ";
-  std::cout << std::endl;
-  std::cout << "Drows: ";
-  std::cout << size(Drows) << " " <<std::endl;
-  //for (const auto& val : Drows) std::cout << val << " ";
-  //std::cout << std::endl;
-  std::cout << std::endl;
-
-  std::cout << "Bvals: ";
-  std::cout << size(Bvals) << " " <<std::endl;
-  std::cout << "Bcols: ";
-  std::cout << size(Bcols) << " " <<std::endl;
-  std::cout << "Brows: ";
-  std::cout << size(Brows) << " " <<std::endl;
-  for (const auto& val : Bcols) std::cout << val << " ";
-  std::cout << std::endl;
-
-  std::cout << "Cvals: ";
-  std::cout << size(Cvals) << " " <<std::endl;
-  std::cout << "Ccols: ";
-  std::cout << size(Ccols) << " " <<std::endl;
-  std::cout << "Crows: ";
-  std::cout << size(Crows) << " " <<std::endl;
-  for (const auto& val : Ccols) std::cout << val << " ";
-  std::cout << std::endl;
-
   std::vector<double> vecRes = loadResVector("vector-Res.bin");
 
-  std::cout << "vecRes: ";
-  std::cout << size(vecRes) << " " <<std::endl;
-  //for (const auto& val : vecRes) std::cout << val << " ";
-  //std::cout << std::endl;
 
-  unsigned int Mb = 16;
-  unsigned int length = 4*Mb;
+  unsigned int Mb = size(Brows)-1 ;
+  unsigned int length = dim_wells*Mb;
   std::vector<double> z1(length, 0.0);          // z1 = B * x
   std::vector<double> z2(length, 0.0);
-  //std::fill(z1.begin(), z1.end(), 0.0);
-  //std::fill(z2.begin(), z2.end(), 0.0);
 
-  for (unsigned int row = 0; row < Mb; ++row) {
-        // for every block in the row
-        for (unsigned int blockID = Brows[row]; blockID < Brows[row + 1]; ++blockID) {
-            unsigned int colIdx = Bcols[blockID];
-            for (unsigned int j = 0; j < dim_wells; ++j) {
-                double temp = 0.0;
-                for (unsigned int k = 0; k < dim; ++k){
-                    //std::cout << colIdx * dim + k << std::endl;
-                    //std::cout << temp << std::endl;
-                }
-                z1[row * dim_wells + j] += temp;
-                //std::cout << z1[row * dim_wells + j] << " ";
-            }
-        }
-    }
-
-/*
+  // B*x multiplication
   for (unsigned int row = 0; row < Mb; ++row) {
         // for every block in the row
         for (unsigned int blockID = Brows[row]; blockID < Brows[row + 1]; ++blockID) {
@@ -146,20 +92,13 @@ int main(int argc, char ** argv)
             for (unsigned int j = 0; j < dim_wells; ++j) {
                 double temp = 0.0;
                 for (unsigned int k = 0; k < dim; ++k) {
-                    std::cout << Bvals[blockID * dim * dim_wells + j * dim + k] << " --- " << std::endl;
                     temp += Bvals[blockID * dim * dim_wells + j * dim + k] * vecRes[colIdx * dim + k];
-                    //std::cout << temp << std::endl;
                 }
                 z1[row * dim_wells + j] += temp;
-                //std::cout << z1[row * dim_wells + j] << " ";
             }
         }
     }
-*/
 
-    // z1 = vecRes;
-
-    std::cout << std::endl;
     std::cout << "\nBx: ";
     std::cout << size(z1) << std::endl;
     for (const auto& val : z1) std::cout << val << " ";
@@ -168,15 +107,24 @@ int main(int argc, char ** argv)
     unsigned int M = dim_wells*Mb;
     void *UMFPACK_Symbolic, *UMFPACK_Numeric;
 
-    (void) umfpack_di_symbolic(M, M, Dcols.data(), Drows.data(), Dvals.data(), &UMFPACK_Symbolic, nullptr, nullptr);
-    (void) umfpack_di_numeric(Dcols.data(), Drows.data(), Dvals.data(), UMFPACK_Symbolic, &UMFPACK_Numeric, nullptr, nullptr);
-    (void) umfpack_di_solve(UMFPACK_A, Dcols.data(), Drows.data(), Dvals.data(), z2.data(), z1.data(), UMFPACK_Numeric, nullptr, nullptr);
+    umfpack_di_symbolic(M, M, Dcols.data(), Drows.data(), Dvals.data(), &UMFPACK_Symbolic, nullptr, nullptr);
+    umfpack_di_numeric(Dcols.data(), Drows.data(), Dvals.data(), UMFPACK_Symbolic, &UMFPACK_Numeric, nullptr, nullptr);
+    umfpack_di_solve(UMFPACK_A, Dcols.data(), Drows.data(), Dvals.data(), z2.data(), z1.data(), UMFPACK_Numeric, nullptr, nullptr);
 
-    std::cout << std::endl;
     std::cout << "\nD-1Bx: ";
     std::cout << size(z2) << std::endl;
     for (const auto& val : z2) std::cout << val << " ";
     std::cout << std::endl;
+
+    std::vector<double> vecSol = loadResVector("vector-Sol.bin");
+    std::vector<double> error(size(vecSol), -1.0);
+    for (int i=0; i<size(z2); i++){
+      error[i]= abs(z2[i]-vecSol[i]);
+      //std::cout << error[i] << " ";
+    }
+    std::cout << std::endl;
+    size_t max_error = *std::max_element(error.begin(),error.end());
+    std::cout << "Inifinity norm of error: " << max_error << std::endl;
 
     umfpack_di_free_symbolic(&UMFPACK_Symbolic);
     umfpack_di_free_numeric(&UMFPACK_Numeric);
--- read-vectors.hpp
+++ read-vectors.hpp
@@ -6,3 +6,46 @@ void loadBCRSMatrixVectors(std::vector<double>& vecVals, std::vector<int>& vecCo
 void loadBCRSMatrixVectors(std::vector<double>& vecVals, std::vector<unsigned int>& vecCols, std::vector<unsigned int>& vecRows, const std::string& filename);
 
 std::vector<double> loadResVector(const std::string& filename);
+
+
+/*
+  std::cout << "Dvals: ";
+  std::cout << size(Dvals) << " " <<std::endl;
+  //for (const auto& val : Dvals) std::cout << val << " ";
+  //std::cout << std::endl;
+  std::cout << "Dcols: ";
+  std::cout << size(Dcols) << " " <<std::endl;
+  for (const auto& val : Dcols) std::cout << val << " ";
+  std::cout << std::endl;
+  std::cout << "Drows: ";
+  std::cout << size(Drows) << " " << std::endl;
+  //for (const auto& val : Drows) std::cout << val << " ";
+  //std::cout << std::endl;
+  std::cout << std::endl;
+
+  std::cout << "Bvals: ";
+  std::cout << size(Bvals) << " " <<std::endl;
+  for (const auto& val : Bvals) std::cout << val << " ";
+  std::cout << std::endl;
+  std::cout << "Bcols: ";
+  std::cout << size(Bcols) << " " <<std::endl;
+  for (const auto& val : Bcols) std::cout << val << " ";
+  std::cout << std::endl;
+  std::cout << "Brows: ";
+  std::cout << size(Brows) << " " <<std::endl;
+  for (const auto& val : Brows) std::cout << val << " ";
+  std::cout << std::endl;
+  std::cout << std::endl;
+
+  std::cout << "Cvals: ";
+  std::cout << size(Cvals) << " " <<std::endl;
+  std::cout << "Ccols: ";
+  std::cout << size(Ccols) << " " <<std::endl;
+  for (const auto& val : Ccols) std::cout << val << " ";
+  std::cout << std::endl;
+  std::cout << "Crows: ";
+  std::cout << size(Crows) << " " <<std::endl;
+  for (const auto& val : Crows) std::cout << val << " ";
+  std::cout << std::endl;
+  std::cout << std::endl;
+*/
